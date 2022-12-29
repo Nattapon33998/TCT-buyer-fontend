97ea3d395359e21d1b88f49a30beb69f3b5d438c
Nattapon Sukthaworn <nattapon33998@gmail.com>
2022-12-29 16:47:31 +0700

    feat: event

--- src/components/PlayGround/index.tsx
+++ src/components/PlayGround/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 
-import { useEtherBalance, useCall, useEthers } from "@usedapp/core";
+import { useEtherBalance, useCall, useEthers, useLogs } from "@usedapp/core";
 import { Interface } from "@ethersproject/abi";
 import productContract from "./productContract.json";
 import { Contract } from "ethers";
 
 const productContractInterface = new Interface(productContract);
-const contractAddress = "0xDAb138B57CF7d669Ef9753bB71a7Bfe3947d9bD8";
+const contractAddress = "0x3895cd100932925Ab586ec109AF2120D4d70ab92";
 const contract = new Contract(contractAddress, productContractInterface);
 
 const Balance = () => {
@@ -20,7 +20,7 @@ const Balance = () => {
   if (!value) {
     return <div> Loading... </div>;
   }
-  return <div> Balance: {parseInt(value[0]._hex, 16)} </div>;
+  return <div> Total Supply: {parseInt(value[0]._hex, 16)} </div>;
 };
 
 const BalanceOf = () => {
@@ -29,7 +29,6 @@ const BalanceOf = () => {
     useCall({ contract, method: "balanceOf", args: [account ?? ""] }) ?? {};
 
   if (error) {
-    // console.log(error.message);
     return <div> fail to load </div>;
   }
 
@@ -39,12 +38,52 @@ const BalanceOf = () => {
   return <div> Balance: {parseInt(value[0]._hex, 16)} </div>;
 };
 
+const Event = () => {
+  const eventLog = useLogs({
+    contract,
+    event: "ProductOwner",
+    args: [0],
+  });
+
+  const { value, error } = eventLog ?? {};
+  console.log("Log event");
+  if (value) {
+    for (let i = 0; i < value.length; i++) {
+      console.log(`เจ้าของคนที่ ${i + 1} : ${value[i].data.owner}`);
+    }
+  }
+
+  if (error) {
+    return <div> fail to load </div>;
+  }
+
+  if (!value) {
+    return <div> Loading... </div>;
+  }
+  return (
+    <div>
+      {value ? (
+        value.map((item, index) => {
+          return (
+            <div key={index}>
+              เจ้าของคนที่ {index + 1} : {item.data.owner}
+            </div>
+          );
+        })
+      ) : (
+        <div> Loading... </div>
+      )}
+    </div>
+  );
+};
+
 const PlayGround: React.FC = () => {
   return (
     <div>
       <p>Play ground</p>
       <Balance />
       <BalanceOf />
+      <Event />
     </div>
   );
 };
