5c59a74852987c423595a6c705ce710bc651dc81
Nattapon Sukthaworn <nattapon33998@gmail.com>
2023-04-14 03:48:57 +0700

    change addr

--- src/config/constants/addresses.ts
+++ src/config/constants/addresses.ts
@@ -1,5 +1,5 @@
 export const ProductContractAddress =
-  "0x49a4776bD0b69C9CF79ae7bcfa772D757F41a621";
+  "0xD25c528118728E62eA14DC164BA47437dCc7Edb7";
 //0x950309a221386535aCc38d9355c4b3123e7224d3
 export const FarmContractAddress = "0xd24728a35a4f61BE1c35F87b8C2430616135786F";
 export const FarmerContractAddress =
@@ -7,4 +7,4 @@ export const FarmerContractAddress =
 export const PlaceContractAddress =
   "0xE9690c9F422e66E89DC508B2cf85DedBa17D615c";
 export const MarketContractAddress =
-  "0x1D2494EF46488A568B4eF6BFb73734EF09FaFF71";
+  "0x1A90a49abd2cA44dBdb0A8985996521857Ba8259";
